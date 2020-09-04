e4bbe9bc9d4e13a3d0a3f355dcaa832b38e45c26
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2020-09-04 03:42:45 +0200

    Added Telegram push message

--- EBG-Contact/README.md
+++ EBG-Contact/README.md
@@ -2,7 +2,7 @@
 Plugin for our contact form on ebg.pw  
 
 ### Requirements
-`npm i joi, express-rate-limit`  
+`npm i joi, express-rate-limit, telebot` 
   
 Add in .env  
 Contact_TG_Bot_Token=YourToken  
--- EBG-Contact/contact.js
+++ EBG-Contact/contact.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const rateLimit = require("express-rate-limit");
 const Joi = require('joi');
+
+const Telebot = require('telebot');
+const bot = new Telebot({
+	token: process.env.Contact_TG_Bot_Token,
+	limit: 1000,
+        usePlugins: ['commandButton']
+});
+
+const PluginConfig = {
+	category: ["Help","Issue","Request","Other"],
+	person: ["All", "GodOfOwls","BolverBlitz","Geozukunft"],
+	UserID: ["Place","357693014","206921999","181585055"]
+}
 
 /* Plugin info*/
 const PluginName = "EBG-contact";
@@ -16,6 +30,7 @@ const limiter = rateLimit({
 
 const router = express.Router();
 
+
 const schema = Joi.object({
 	name: Joi.string().trim().required(),
 	email: Joi.string().trim().required(),
@@ -27,6 +42,15 @@ const schema = Joi.object({
 router.post('/', limiter, async (reg, res, next) => {
 	try{
 		const value = await schema.validateAsync(reg.body);
+		if(value.person === 0){
+			var Mention = "";
+			for (i = 1; i < PluginConfig.UserID.length; i++) {
+				Mention += `<a href="tg://user?id=${PluginConfig.UserID[i]}"> </a>`
+			}
+		}else{
+			var Mention = `<a href="tg://user?id=${PluginConfig.UserID[value.person]}"> </a>`
+		}
+		bot.sendMessage(`-251992918`, `Neue Kontaktanfrage von ${value.name}\nKategorie: ${PluginConfig.category[value.category]}\nAnfrage für ${PluginConfig.person[value.person]}\n${Mention}\n<b>Nachricht:</b>\n<pre language="c++">${value.message}</pre>`, { parseMode: 'html' , webPreview: false}).catch(error => console.log('Error: (Telegram Send Message)', error.description));
 		res.json(value);
 	}catch (error) {
 		next(error);
